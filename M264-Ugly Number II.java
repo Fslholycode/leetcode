656289d895f21903313e0ce0c5e5dc8169a4285a
Fslholycode <fslholy@163.com>
2017-04-13 20:30:03 +0800

    priorityqueue 记得转换成long，还有注意加入重复元素，要判断是否重复

---
+++ M264-Ugly Number II.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int nthUglyNumber(int n) {
+        long result = 0;
+        PriorityQueue<Long>  pq = new PriorityQueue<Long>();
+        pq.add((long) 1);
+        while (n > 0) {
+            result = pq.poll();
+            if (!pq.contains(result*2)) {pq.add(result*2);}
+            if (!pq.contains(result*3)) {pq.add(result*3);}
+            if (!pq.contains(result*5)) {pq.add(result*5);}
+            n--;
+        }
+        return (int) result;
+    }
+    public static void main (String[] args) {
+    	    System.out.println(new Solution().nthUglyNumber(1600));
+    }
+}
