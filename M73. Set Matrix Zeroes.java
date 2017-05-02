df5d80612498847753f02543c321fc6dacdbce2a
Fslholycode <fslholy@163.com>
2017-05-02 10:50:48 +0800

    Use the first column and the first row as marker:
    first scan through the whole matrix, and if one row i has zero, label
    matrix[i][0] = 0, if column j has zero, then label matrix[0][j] = 0.
    if we find the first row has zero, then mark a boolean row = true, if
    the first column has zeros, mark a boolean col = true;
    
    By the markers on the first row and first col, set the other columns
    and rows to zeros. (first row and first column already contain zeros)
    
    According to booleans row and col, decide whether to set first row and
    column to zeros.

---
+++ M73. Set Matrix Zeroes.java
@@ -0,0 +1,44 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean fr = false;
+        boolean fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                    if (i == 0) {
+                        fr = true;
+                    } 
+                    if (j == 0) {
+                        fc = true;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 0; j < matrix.length; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        if (fr) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
