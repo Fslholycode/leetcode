8016f345b59b44ebd1fef7ce440130ca750a795b
Fslholycode <fslholy@163.com>
2017-05-12 15:41:22 +0800

    divide to two groups, A+B, C+D,

---
+++ M454. 4Sum II.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                int sum = A[i]+B[j];
+                map.put(sum, map.getOrDefault(sum,0)+1);
+            }
+        }
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < D.length; j++) {
+                int sum = C[i]+D[j];
+                count += map.getOrDefault(-sum,0);
+            }
+        }
+        return count;
+    }
+}
