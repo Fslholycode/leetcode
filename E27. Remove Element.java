4d0f86c6f061ae08e946d23e4c4321266fbfb33e
Fslholycode <fslholy@163.com>
2017-06-04 10:26:30 +0800

    EASY

--- E27. Remove Element.java
+++ E27. Remove Element.java
@@ -1,23 +1,12 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
         if (nums == null || nums.length == 0) return 0;
-        int start = 0;
-        int end = nums.length-1;
-        while (start < end) {
-            while (end >= 0 && nums[end] == val) {
-                end--;
+        int len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[len++] = nums[i];
             }
-            if (end > start && nums[start] == val) {
-                int temp = nums[end];
-                nums[start] = temp;
-                nums[end] = val;
-            }
-            start++;
         }
-        int i = nums.length-1;
-        while (i >= 0 && nums[i] == val) {
-            i--;
-        }
-        return i+1;
+        return len;
     }
 }
