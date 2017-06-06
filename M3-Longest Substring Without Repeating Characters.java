5e88a156ea6ffb5603921bbd03fbe9a7daf997cb
Fslholycode <fslholy@163.com>
2017-06-06 11:37:23 +0800

    The idea is use a hash set to track the longest substring without repeating characters so far, use a fast pointer j  to see if character j is in the hash set or not, if not, great, add it to the hash set, move j forward and update the max length, otherwise, delete from the head by using a slow pointer i until we can put character j to the hash set.

--- M3-Longest Substring Without Repeating Characters.java
+++ M3-Longest Substring Without Repeating Characters.java
@@ -1,33 +1,18 @@
-import java.util.Hashtable;
-
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) return 0;
-        int maxLength = 1;
-        int index1 = 0;
-        int index2 = -1;
-        int nowmax = 0;
-        Hashtable<Character,Integer> ht = new Hashtable<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            char key = s.charAt(i);
-            if (ht.containsKey(key)) {
-                maxLength = Math.max(nowmax, maxLength);
-                index1 = Math.max(ht.get(key)+1,index1);
-                index2++;
-                nowmax = index2-index1+1;
-                ht.remove(key);
-                ht.put(key,i);
-            }
-            else {
-                ht.put(key, i);
-                nowmax++;
-                index2++;
+        int slow = 0, fast = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+        while (fast < s.length()) {
+            char c = s.charAt(fast);
+            if (set.contains(c)) {
+                set.remove(s.charAt(slow));
+                slow++;
+            } else {
+                set.add(c);
+                max = Math.max(max,fast-slow+1);
+                fast++;
             }
         }
-        return Math.max(maxLength, nowmax);
-    }
-    public static void main(String[] args) {
-    	    String a = "abba";
-    	    System.out.println(new Solution().lengthOfLongestSubstring(a));
+        return max;
     }
 }
