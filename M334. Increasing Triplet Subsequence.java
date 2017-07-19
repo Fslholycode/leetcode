8cf7449664e94a4b3f52be7d024b00423e6fc409
Fslholycode <fslholy@163.com>
2017-07-19 17:57:15 +0800

    if (nums[i] <= n1) n1 = nums[i];             else if (nums[i] <= n2) n2 = nums[i];             else return true;

---
+++ M334. Increasing Triplet Subsequence.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n1 = Integer.MAX_VALUE, n2 = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= n1) n1 = nums[i];
+            else if (nums[i] <= n2) n2 = nums[i];
+            else return true;
+        }
+        return false;
+    }
+}
