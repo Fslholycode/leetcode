60df17f8f6f0a04bde5fc7772e56ac19d964809d
Fslholycode <fslholy@163.com>
2017-09-01 11:26:08 +0800

    1.merge 递归 l1.next = merge(l1.next, l2);             return l1; 2.l1, l2 递归

---
+++ H23. Merge k Sorted Lists.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return partition(lists, 0, lists.length-1);
+    }
+    public ListNode partition(ListNode[] lists, int n, int m) {
+        if (n == m) return lists[n];
+        if (n > m) return null;
+        int mid = (n+m)/2;
+        ListNode l1 = partition(lists, n, mid);
+        ListNode l2 = partition(lists, mid+1 ,m);
+        return merge(l1,l2);
+    }
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = merge(l2.next, l1);
+            return l2;
+        }
+    }
+}
