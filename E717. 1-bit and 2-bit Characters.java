3a3d40992272e2a2026b9d7d966b0d06122d5182
Fslholycode <fslholy@163.com>
2017-10-30 15:10:54 -0700

    easy

---
+++ E717. 1-bit and 2-bit Characters.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        for (int i = 0; i < bits.length; i++) {
+            if (bits[i] == 1) {
+                i = i+1;
+                if (i == bits.length-1) return false;
+                continue;
+            }
+        }
+        return true;
+    }
+}
