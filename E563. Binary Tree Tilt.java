ce6eeb2a1326584b61ed567fea04a915353fa426
Fslholycode <fslholy@163.com>
2017-11-01 11:49:39 -0700

    recursive int left = helper(root.left);         int right = helper(root.right);         res += Math.abs(left-right);         return left+right+root.val;

---
+++ E563. Binary Tree Tilt.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int findTilt(TreeNode root) {
+        helper(root);
+        return res;
+    }
+    public int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        res += Math.abs(left-right);
+        return left+right+root.val;
+    }
+}
