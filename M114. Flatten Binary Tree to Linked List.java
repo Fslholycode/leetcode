2b25de089f0334e931b67abcc67bcd6822d68877
Fslholycode <fslholy@163.com>
2017-11-06 22:39:15 -0800

    while (cur.right != null) cur = cur.right;         cur.right = right;

---
+++ M114. Flatten Binary Tree to Linked List.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        flatten(left);
+        flatten(right);
+        root.left = null;
+        root.right = left;
+        TreeNode cur = root;
+        while (cur.right != null) cur = cur.right;
+        cur.right = right;
+    }
+}
