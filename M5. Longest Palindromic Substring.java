c0c6d922f0a805fbe56f3dbdf7373c2b70c7b54e
Fslholycode <fslholy@163.com>
2017-11-11 11:08:40 -0800

    用hashmap记录之前出现的所有的index，然后用一个矩阵标记从i到j是回文的，进行推导更新

--- M5. Longest Palindromic Substring.java
+++ M5. Longest Palindromic Substring.java
@@ -1,24 +1,33 @@
 public class Solution {
     public String longestPalindrome(String s) {
-        String res = "";
-        int cur = 0;
-        for(int i=0;i<s.length();i++) {
-            if (isPalindrome(s, i-cur-1, i)) {
-                res = s.substring(i-cur-1, i+1);
-                cur = cur+2;
-            }
-            else if (isPalindrome(s, i-cur, i)) {
-                res = s.substring(i-cur, i+1);
-                cur = cur+1;
+        int len = 1;
+        if (s.length() <= 1) return s;
+        String res = s.substring(0,1);
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
+        }
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                List<Integer> prev = map.get(c);
+                for (int index : prev) {
+                    if (index + 1 == i || dp[index+1][i-1]) {
+                        dp[index][i] = true;
+                        if (i-index+1 > len) {
+                            res = s.substring(index,i+1);
+                            len = i-index+1;
+                        }
+                         
+                    }
+                }
+                map.get(c).add(i);
+            } else {
+                map.put(c, new ArrayList());
+                map.get(c).add(i);
             }
         }
         return res;
-    }
-    public boolean isPalindrome(String s, int begin, int end){
-        if(begin<0) return false;
-        while(begin<end){
-        	if(s.charAt(begin++)!=s.charAt(end--)) return false;
-        }
-        return true;
     }
 }
