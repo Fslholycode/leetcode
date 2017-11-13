fe9f762d29c9727fdb3ac5a5cf2e53ac0481ad45
Fslholycode <fslholy@163.com>
2017-11-13 13:10:55 -0800

    https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/dis cuss/

---
+++ H440. K-th Smallest in Lexicographical Order.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int cur = 1;
+        k--;
+        while (k > 0) {
+            int steps = calSteps(n, cur, cur+1);
+            if (steps > k) {
+                cur *= 10;
+                k -= 1;
+            } else {
+                k -= steps;
+                cur++;
+            }
+        }
+        return cur;
+    }
+    public int calSteps(int n, long n1, long n2) {
+        int steps = 0;
+        while (n1 <= n) {
+            steps += Math.min(n+1, n2)-n1;
+            n1*=10;
+            n2*=10;
+        }
+        return steps;
+    }
+}
