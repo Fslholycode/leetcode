2984e3bed1e6a4d4fc3803a9011ccf21039160e9
Fslholycode <fslholy@163.com>
2018-05-16 18:41:23 -0700

    n-1个数加1相当于其中一个数减1.。。。

---
+++ E453. Minimum Moves to Equal Array Elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    int count = 0;
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num:nums) {
+            min = Math.min(min, num);
+        }
+        int count = 0;
+        for (int num: nums) {
+            count+=num-min;
+        }
+        return count; 
+    }
+
+}
