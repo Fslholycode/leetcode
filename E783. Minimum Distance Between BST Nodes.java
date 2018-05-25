b9d5229d79b27f44a0e3a3a16532661bca0db9eb
Fslholycode <fslholy@163.com>
2018-05-24 23:18:33 -0700

    inorder 需要习惯下，先left 然后pre！= null，然后设置pre=root，然后right递归就行

---
+++ E783. Minimum Distance Between BST Nodes.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode pre = null;
+    int res = Integer.MAX_VALUE;
+    public int minDiffInBST(TreeNode root) {
+        if (root.left != null) minDiffInBST(root.left);
+        if (pre != null) res = Math.min(res, root.val-pre.val);
+        pre = root;
+        if (root.right != null) minDiffInBST(root.right);
+        return res;
+    }
+}
