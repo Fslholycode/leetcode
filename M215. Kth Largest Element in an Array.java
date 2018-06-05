1937118fb03b558a57bac8f80f2294aba5e9c6a4
Fslholycode <fslholy@163.com>
2018-06-05 11:56:19 -0700

    1.shuffle 2.快速选择的时候要注意因为重复数字陷入死循环，这时候可以将最后的一个数字去掉。

--- M215. Kth Largest Element in an Array.java
+++ M215. Kth Largest Element in an Array.java
@@ -1,22 +1,45 @@
+import java.util.Random;
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int[] max = new int[k];
-        for (int i = 0; i < k; i++) {
-            max[i] = Integer.MIN_VALUE;
+        shuffle(nums);
+        return partition(nums, 0, nums.length - 1, k);
+    }
+    
+    public int partition(int[] nums, int start, int end, int k) {
+    		Random rand = new Random();
+        int index = rand.nextInt(end - start + 1) + start;
+        int pivot = nums[index];
+        swap(nums, index, end);
+        index = start;
+        for (int i = start; i <= end; i++) {
+        		if (nums[i] >= pivot) swap(nums, index++, i);
         }
-        int i = 0;
-        while (i < nums.length) {
-            for (int j = k-1; j >= 0; j--) {
-                if (nums[i] > max[j]) {
-                    for (int m = 0; m <= j-1; m++) {
-                        max[m] = max[m+1];
-                    }
-                    max[j] = nums[i];
-                    break;
-                }
-            }
-            i++;
+        if (index-start == k) return pivot;
+        else if (index- start > k) return partition(nums, start, Math.min(end-1, index),k);
+        else return partition(nums, index, end, k-index+start);
+        
+    }
+    
+
+    public void shuffle(int[] nums) {
+        Random random = new Random();
+        for (int i = nums.length-1; i >= 0; i--) {
+            int j = random.nextInt(i+1);
+            swap(nums, i, j);
         }
-        return max[0];
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void main(String[] args) {
+    		int[] nums = {99,99};
+    		int k = 1;
+    		System.out.println(new Solution().findKthLargest(nums, k));
+    		for (int i = 0; i < nums.length; i++) {
+    			System.out.println(nums[i]);
+    		}
     }
 }
