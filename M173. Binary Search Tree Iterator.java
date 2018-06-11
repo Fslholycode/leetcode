bb287e687c9a0f6fd6f0675cb4bbb5d4012d2d95
Fslholycode <fslholy@163.com>
2018-06-11 11:26:23 -0700

    用一个stack保存这个数所有的左边的孩子，每次取出一个结点就把这个结点的右子树的左孩子都存进去。

---
+++ M173. Binary Search Tree Iterator.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+    Stack<TreeNode> s = new Stack();
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !s.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = s.pop();
+        pushAll(node.right);
+        return node.val;
+    }
+    public void pushAll(TreeNode root) {
+        while (root != null) {
+            s.push(root);
+            root = root.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
