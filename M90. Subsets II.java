086a73a8b67e560521c65f9656fad731a52bf0d7
Fslholycode <fslholy@163.com>
2018-07-17 20:49:55 +0800

    sort and then use dfs, everytime check the previous removed one and the current one.

--- M90. Subsets II.java
+++ M90. Subsets II.java
@@ -1,30 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
         Arrays.sort(nums);
-        int num = nums[0]-1;
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        Helper(res, nums, new ArrayList<Integer>(), 0, num);
-        res.add(new ArrayList<Integer>());
+        helper(nums, res, new ArrayList(), 0, null);
         return res;
     }
-    public void Helper(List<List<Integer>> res, int[] nums, ArrayList<Integer> list, int pos, int num) {
-        for (int i = pos; i < nums.length; i++) {
-            if (num == nums[i]) {
-                continue;
-            }
+    public void helper(int[] nums, List<List<Integer>> res, List<Integer> list, int pos, Integer pre) {
+        res.add(new ArrayList(list));
+        if (pos >= nums.length) return;
+        for (int i = pos; i < nums.length; i++){
+            if (pre != null && pre == nums[i]) continue;
             list.add(nums[i]);
-            res.add(new ArrayList<>(list));
-            Helper(res, nums, list, i+1,num);
-            num = list.get(list.size()-1);
+            helper(nums, res, list, i+1, pre);
+            pre = list.get(list.size()-1);
             list.remove(list.size()-1);
         }
-    }
-    public static void main(String[] args) {
-    	    int[] a = {1,2,2};
-    	    System.out.println(new Solution().subsetsWithDup(a));
     }
 }
