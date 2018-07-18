13e411fad8c0e802fcc3796d7dd3790be515885f
Fslholycode <fslholy@163.com>
2018-07-18 10:33:17 +0800

    设置一个数组用来保存每条边，不需要往回遍历，只需要遍历四条边，对于每一个位置的数字。

--- M473. Matchsticks to Square.java
+++ M473. Matchsticks to Square.java
@@ -1,29 +1,29 @@
 class Solution {
     public boolean makesquare(int[] nums) {
-        if (nums.length < 4) return false;
+        if (nums.length == 0) return false;
+        int sum = 0;
+        for (int num:nums) sum+=num;
+        if (sum%4 != 0) return false;
+        int side = sum/4;
         Arrays.sort(nums);
         reverse(nums);
-        int sum = 0;
-        for (int num:nums) {
-            sum += num;
-        }
-        int target = sum/4;
-        if (sum%4 != 0) return false;
+        boolean[] visited = new boolean[nums.length];
         int[] each = new int[4];
-        return dfs(nums, target, each, 0);
+        return helper(nums, side,each, 0);
     }
-    public boolean dfs(int[] nums, int target, int[] each, int pos) {
-        if (pos == nums.length-1) {
-            if (each[0] == target && each[1] == target && each[2] == target) return true;
+    public boolean helper(int[] nums, int side, int[] each, int pos) {
+        if (pos == nums.length) {
+            if (each[0] == side && each[1] == side && each[2] == side) return true;
             return false;
         }
         for (int i = 0; i < 4; i++) {
-            if (nums[pos] + each[i] > target) continue;
+            if (nums[pos] +each[i] > side) continue;
             each[i] += nums[pos];
-            if (dfs(nums, target, each, pos+1)) return true;
+            if (helper(nums, side, each, pos+1)) return true;
             each[i] -= nums[pos];
         }
         return false;
+        
     }
     private void reverse(int[] nums) {
         int i = 0, j = nums.length - 1;
