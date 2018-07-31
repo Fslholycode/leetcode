100a26e5ecf8d0332e21811d1f93ad9f812bc7ec
Fslholycode <fslholy@163.com>
2018-07-31 21:12:58 +0800

    每个都标记用了还是没用。 res[0] = Math.max(left[0],left[1])+Math.max(right[0], right[1]);         res[0] = Math.max(left[1],left[0])+Math.max(right[1], right[0]);

--- M337. House Robber III.java
+++ M337. House Robber III.java
@@ -7,17 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+class Solution {
     public int rob(TreeNode root) {
-        int[] res = robsub(root);
+        if (root == null) return 0;
+        int[] res = helper(root);
         return Math.max(res[0], res[1]);
     }
-    public int[] robsub(TreeNode root) {
+    
+    public int[] helper(TreeNode root) {
         if (root == null) return new int[2];
-        int[] left = robsub(root.left);
-        int[] right = robsub(root.right);
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
         int[] res = new int[2];
-        res[0] = Math.max(left[0],left[1])+Math.max(right[0], right[1]);
+        res[0] = Math.max(left[1],left[0])+Math.max(right[1], right[0]);
         res[1] = root.val+left[0]+right[0];
         return res;
     }
