2dcb7edb7969453a776e96e999cae8dfa294b35c
Fslholycode <fslholy@163.com>
2018-08-01 23:12:20 +0800

    dp[i][j] = Math.max(Math.min(dp[i+1][j-1], dp[i+2][j]) + nums[i], Math.min(dp[i][j-2], dp[i+1][j - 1]) + nums[j]); 初始化，只剩下最后一个和最后两个的情况。

--- M486. Predict the Winner.java
+++ M486. Predict the Winner.java
@@ -1,8 +1,20 @@
-public class Solution {
+class Solution {
     public boolean PredictTheWinner(int[] nums) {
-        return helper(nums, 0, nums.length-1)>=0;
-    }
-    private int helper(int[] nums, int s, int e){  
-        return s==e?nums[e] :Math.max(nums[e]-helper(nums,s,e-1),nums[s]-helper(nums,s+1,e));
+        int sum = 0;
+        int len = nums.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = nums[i];
+            sum+=nums[i];
+        }
+        for (int i = 0; i < len-1; i++) {
+            dp[i][i+1] = Math.max(nums[i],nums[i+1]);
+        }
+        for (int j = 2; j < len; j++) {
+            for (int i = j-2; i >= 0; i--) {
+                dp[i][j] = Math.max(Math.min(dp[i+1][j-1], dp[i+2][j])+nums[i], Math.min(dp[i+1][j-1], dp[i][j-2])+nums[j]);
+            }
+        }
+        return 2*dp[0][len-1] >= sum;
     }
 }
