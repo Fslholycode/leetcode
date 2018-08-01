0cd9469c91e3655517ada14114c1abd2746faa54
Fslholycode <fslholy@163.com>
2018-08-01 09:44:05 +0800

    用一个数组dp[i] means the tail of all the length i of increasing subsequence. then len = 1   :      [4], [5], [6], [3]   => tails[0] = 3 len = 2   :      [4, 5], [5, 6]       => tails[1] = 5 len = 3   :      [4, 5, 6]            => tails[2] = 6 We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
    (1) if x is larger than all tails, append it, increase the size by 1
    (2) if tails[i-1] < x <= tails[i], update tails[i]

--- M300. Longest Increasing Subsequence.java
+++ M300. Longest Increasing Subsequence.java
@@ -1,22 +1,19 @@
-public class Solution {
+class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums.length <= 1) return nums.length;
-        int[] dp = new int[nums.length+1];
-        dp[0]= 0;
-        dp[1] = 1;
-        int len = 1;
-        for (int i = 2; i < nums.length+1; i++) {
-            int index = i-1;
-            dp[i] = 1;
-            while (index > 0) {
-                if (nums[index-1] < nums[i-1]) {
-                    dp[i] = Math.max(dp[i], dp[index]+1);
-                }
-                index--;
+        int len = nums.length;
+        int[] dp = new int[len];
+        int size = 0;
+        for (int num:nums) {
+            int left = 0;
+            int right = size;
+            while (left < right) {
+                int mid = left+(right-left)/2;
+                if (dp[mid] < num) left = mid+1;
+                else right = mid;
             }
-            len = Math.max(len,dp[i]);
+            dp[left] = num;
+            if (left == size) size++;
         }
-        return len;
+        return size;
     }
-    
 }
